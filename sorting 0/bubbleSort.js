398ef991fcaf08c84a704ffc86b0f67e0fccc3f3
Ayush Ojha <ayushojha1512@gmail.com>
2025-07-08 12:03:22 +0530

    Added bubbleSort Algo

---
+++ sorting 0/bubbleSort.js
@@ -0,0 +1,18 @@
+let arr = [5, 4, 3, 2, 1];
+
+function bubbleSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(arr));
